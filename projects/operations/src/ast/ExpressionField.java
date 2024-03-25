3867197ae16d080abbd69e58bb950fe358fcf180
unknown <tnguy316@uottawa.ca>
2024-03-25 15:41:20 -0400

    Implementation of ExpressionField, ExpressionValue, Operator, and UnaryExpression

--- projects/operations/src/ast/ExpressionField.java
+++ projects/operations/src/ast/ExpressionField.java
@@ -1,4 +1,37 @@
 package ast;
 
-public class ExpressionField {
+/*This import error isn't resolved yet. Need to find an import path to go to 'visitor' folder to import ExpressionVisitor*/
+import visitor.ExpressionVisitor;
+
+public class ExpressionField implements Expression {
+    private String fieldName;
+
+    public ExpressionField(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    /**
+     * Returns the field name.
+     * @return
+     */
+    @Override
+    public String infix() {
+        return fieldName;
+    }
+
+    /**
+     * This method accepts a expression visitor and calls
+     * the visit method on the visitor passing itself.
+     * @param visitor
+     *        ExpressionVisitor for traversing the expression tree.
+     * @param data
+     *        Buffer to hold the result of process node.
+     * @param <T>
+     * @return
+     *        Returns the processed data.
+     */
+    @Override
+    public <T> T accept(ExpressionVisitor visitor, T data) {
+        return (T)visitor.visitExpressionField(this, data);
+    }
 }
--- projects/operations/src/ast/UnaryExpression.java
+++ projects/operations/src/ast/UnaryExpression.java
@@ -1,4 +1,46 @@
 package ast;
 
-public class UnaryExpression {
+/*This import error isn't resolved yet. Need to find an import path to go to 'visitor' folder to import ExpressionVisitor*/
+import visitor.ExpressionVisitor;
+
+/**
+ * This class represents an UnaryExpression node
+ * in the expression tree with just NOT operator
+ * and a single operand which can either be a
+ * BinaryExpression or CompoundExpression.
+ *
+ * @author sjaiswal
+ */
+public class UnaryExpression extends AbstractExpression{
+
+    /**
+     * Default constructor.
+     */
+    public UnaryExpression() {
+        super();
+    }
+
+    /**
+     * Parameterized constructor.
+     *
+     * @param leftOperand
+     * @param operator
+     * @param rightOperand
+     */
+    public UnaryExpression(Expression leftOperand, Operator operator, Expression rightOperand) {
+        super(leftOperand, operator, rightOperand);
+    }
+
+    /**
+     * This method accepts a expression visitor and calls
+     * the visit method on the visitor passing itself.
+     *
+     * @param visitor ExpressionVisitor for traversing the expression tree.
+     * @param data    Buffer to hold the result of process node.
+     * @return Returns the processed data.
+     */
+    @Override
+    public <T> T accept(ExpressionVisitor visitor, T data) {
+        return (T)visitor.visitUnaryExpression(this, data);
+    }
 }
